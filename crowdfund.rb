60575e3841396e1e65c3c7896e09c2a9276922b0
Roland Wettstein <rwettstein@rallydev.com>
2013-07-12 17:40:52 -0600

    Adding New Functionality

---
+++ crowdfund.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+project_1 = "ABC"
+project_2 = "LMN"
+project_3 = "XYZ"
+
+project_1_funding = 1000
+projecrt_2_funding = 500
+project_3_funding = 150
+
+puts "Project #{project_1} has $#{project_1_funding} in funding"
+puts
+puts "Projects: \nProject #{project_1} \nProject #{project_2} \nProject #{project_3}"
--- player.rb
+++ player.rb
@@ -1,8 +1,47 @@
 #!/usr/bin/ruby
 
-name = "Roland"
-health = 10
+player_1 = "Roland"
+player_2 = "Thomas"
+player_3 = "Guido"
+player_4 = "Andre"
+player_health_1 = 10
+player_health_2 = 20
+player_health_3 = 30
+player_health_4 = 40
 
-puts "The player's name is #{name}"
-puts "The player's health is #{health}"
-puts "The player's \t\tname is #{name} and her/his health is \n\n#{health}"
+puts "The player's name is #{player_1}"
+puts "The player's health is #{player_health_1}"
+puts "The player's \t\tname is #{player_1} and her/his health is \n\n#{player_health_1*3}"
+puts
+puts "The players names are: \n\tRoland \n\tThomas \n\tGuido"
+puts
+puts "The Players are \n\t#{player_1} \n\t#{player_2} \n\t#{player_3}"
+puts
+puts "#{player_1} has a health value of #{player_health_1}"
+puts "#{player_2} has a health value of #{player_health_2}"
+puts "*************#{player_3} has a health value of #{player_health_3}*****************"
+puts "#{player_4} ......................... #{player_health_4} health"
+puts
+
+puts "#{player_1.capitalize}"
+puts "#{player_1.downcase}"
+puts "#{player_1.upcase}"
+puts "#{player_1.swapcase}"
+
+puts "#{player_1.capitalize} has a health value of #{player_health_1}"
+puts "#{player_2.upcase} has a health value of #{player_health_2}"
+puts "#{player_3.downcase} has a health value of #{player_health_3}"
+puts "#{player_4.swapcase} ......................... #{player_health_4} health"
+puts
+player_health_2 = player_health_1
+puts "Player #{player_1} has a health value of #{player_health_1}"
+puts "Player #{player_2} has a health value of #{player_health_2}"
+puts
+player_health_1 = 30
+puts "Player #{player_1} has a health value of #{player_health_1}"
+puts "Player #{player_2} has a health value of #{player_health_2}"
+puts
+puts "Player #{player_3.center(10, '*')} has a health value of #{player_health_3}"
+puts
+input = "Player #{player_3} has a health value of #{player_health_3}"
+puts "#{input.center(50, '*')}"
